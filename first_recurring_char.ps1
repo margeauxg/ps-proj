a85a8aa3bd3e0777eaa6f394a3106671cae76d59
margeauxg <38711220+margeauxg@users.noreply.github.com>
2020-10-30 16:05:34 -0700

    Create first_recurring_char.ps1

---
+++ first_recurring_char.ps1
@@ -0,0 +1,26 @@
+﻿function Output_RepeatChar {
+param ( [string]$foo )
+
+    $char_hash = @{}
+    $char_array = $foo.ToCharArray()
+
+    foreach ($char in $char_array) {
+        if ($char_hash.ContainsKey($char)) {
+
+        return $char
+
+        <# if you want to exclude spaces use this instead
+        if ($char -ne " ") {
+            return $char
+        }
+        #>
+
+        } else {
+            $char_hash.Add($char, 1)
+        }
+    }
+
+}
+
+$foo = "abcdedcba"
+Output_RepeatChar $foo
